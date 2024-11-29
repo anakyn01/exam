7afe6dc1e52772c426ca7b3fb7e2fd99261b280f
hwang <anakyn@naver.com>
2024-11-29 17:29:35 +0900

    1129 last

---
+++ gasstation_12345678/src/main/java/gas/Gas.java
@@ -0,0 +1,56 @@
+package gas;
+
+public class Gas {
+
+	private int bno;
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	private String custno;
+	private String custname;
+	private String custjumin;
+	private String custtel1;
+	private String custtel2;
+	private String custtel3;
+	public String getCustno() {
+		return custno;
+	}
+	public void setCustno(String custno) {
+		this.custno = custno;
+	}
+	public String getCustname() {
+		return custname;
+	}
+	public void setCustname(String custname) {
+		this.custname = custname;
+	}
+	public String getCustjumin() {
+		return custjumin;
+	}
+	public void setCustjumin(String custjumin) {
+		this.custjumin = custjumin;
+	}
+	public String getCusttel1() {
+		return custtel1;
+	}
+	public void setCusttel1(String custtel1) {
+		this.custtel1 = custtel1;
+	}
+	public String getCusttel2() {
+		return custtel2;
+	}
+	public void setCusttel2(String custtel2) {
+		this.custtel2 = custtel2;
+	}
+	public String getCusttel3() {
+		return custtel3;
+	}
+	public void setCusttel3(String custtel3) {
+		this.custtel3 = custtel3;
+	}
+	
+	
+}
---
+++ gasstation_12345678/src/main/java/gas/Money.java
@@ -0,0 +1,68 @@
+package gas;
+
+public class Money {
+
+	private int salebno;
+	private String saleno;
+	private String oildate;
+	private String oiltype;
+	private int amount;
+	private String paytype;
+	private String custno;
+	private String creditcard;
+	private int oilcost;
+	public int getSalebno() {
+		return salebno;
+	}
+	public void setSalebno(int salebno) {
+		this.salebno = salebno;
+	}
+	public String getSaleno() {
+		return saleno;
+	}
+	public void setSaleno(String saleno) {
+		this.saleno = saleno;
+	}
+	public String getOildate() {
+		return oildate;
+	}
+	public void setOildate(String oildate) {
+		this.oildate = oildate;
+	}
+	public String getOiltype() {
+		return oiltype;
+	}
+	public void setOiltype(String oiltype) {
+		this.oiltype = oiltype;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public String getPaytype() {
+		return paytype;
+	}
+	public void setPaytype(String paytype) {
+		this.paytype = paytype;
+	}
+	public String getCustno() {
+		return custno;
+	}
+	public void setCustno(String custno) {
+		this.custno = custno;
+	}
+	public String getCreditcard() {
+		return creditcard;
+	}
+	public void setCreditcard(String creditcard) {
+		this.creditcard = creditcard;
+	}
+	public int getOilcost() {
+		return oilcost;
+	}
+	public void setOilcost(int oilcost) {
+		this.oilcost = oilcost;
+	}
+}
