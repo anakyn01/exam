27b61e90b3855eec47e48f36f810e2d2b7f85866
hbk <anakyn@naver.com>
2025-12-05 16:32:38 +0900

    20251205

---
+++ Exam_13.java
@@ -0,0 +1,10 @@
+public class Exam_13{
+
+    public static void main(String[] args){
+        int i;
+        int[] a = {0,1,2,3};
+        for(i=0; i < 4; i++){
+            System.out.print(a[i] + " "); //0 1 2 3
+        }
+    }
+}
