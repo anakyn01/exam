284aa4a401db2c077a4023756b465291286fb234
hbk <anakyn@naver.com>
2025-12-09 09:15:13 +0900

    20251209

---
+++ Exam_19.java
@@ -0,0 +1,24 @@
+class A{
+    private int a;
+    public A(int a){
+        this.a = a;
+    }
+    public void display(){
+        System.out.println("a=" + a);
+    }
+
+}
+
+class B extends A{
+    public B(int a){
+        super(a);
+        super.display();
+    }
+}
+
+
+public class Exam_19 {
+    public static void main(String[] args){
+    B obj = new B(10);
+    }
+}
---
+++ Exam_5.java
@@ -0,0 +1,17 @@
+class Parent{
+    public void show(){
+        System.out.println("Parent");
+    }
+}
+
+class Child extends Parent{
+    public void show(){
+        System.out.println("Child");
+    }
+}
+public class Exam_5 {
+    public static void main(String[] args){
+        Parent pa = new Child();
+        pa.show();
+    }
+}
