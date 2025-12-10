5b450abbbc2b6dfc4a787a4fbbf7a0a86298d0f9
hbk <anakyn@naver.com>
2025-12-10 13:59:04 +0900

    20251210- 점심

---
+++ 5_.py
@@ -0,0 +1,8 @@
+class good:
+    li = ["seoul","kyeonggi","inchon","daejeon","daegu","pusan"]
+g = good()
+str01 = ''
+for i in g.li:
+    str01 = str01 + i[0]
+print(str01) #skiddp
+#각단어들의 0번째 (첫번째 글자)row 출력
---
+++ Seven_7.java
@@ -0,0 +1,10 @@
+public class Seven_7 {
+    public static void main(String[] args){
+        int[][]arr = new int[][]{{45,50,75},{89}};
+        System.out.println(arr[0].length); //3
+        System.out.println(arr[1].length); //1
+        System.out.println(arr[0][0]);//45
+        System.out.println(arr[0][1]);//50
+        System.out.println(arr[1][0]);//89
+    }
+}
